ed5a287e0b32185ca4a705e872d224ffafc69c34
Luis Felipe <luisfelipemathiasleite@gmail.com>
2025-11-17 16:46:19 -0300

    Update aula07.py

--- aula07.py
+++ aula07.py
@@ -51,28 +51,68 @@ os.system("color 2")
 # print(RetornaMult(float(input("Digite o 1° numero: ")),float(input("Digite o 2° numero: "))))
 
 #ex02
-leo=0
-def DolarReal():
-    print(f"R$ {leo/5.30}")
-def RealDolar():
-    print(f"U$ {leo*5.30}")
-print("="*8, "CONVERSÃO FODASTICA 3000", "="*8)
-print("1 - Converter para DOLAR")
-print("2 - Converter para REAL")
-print("3 - Sair do programa")
-print("="*42)
-opcao=int(input("\nDigite uma opção: "))
-while opcao!=3:
-    
-    if opcao == 1:
-        leo = float(input("Digite o valor R$"))
-        RealDolar()
-    elif opcao == 2:
-        leo = float(input("Digite o valor U$"))
-        DolarReal()
-    else:
-        print("OPÇÃO INVÁLIDA")
-    opcao=int(input("\nDigite uma opção: "))
+# leo=0
+# opcao=0
+# def DolarReal():
+#     leo = float(input("Digite o valor U$"))
+#     print(f"R$ {leo/5.30}")
+# def RealDolar():
+#     leo = float(input("Digite o valor R$"))
+#     print(f"U$ {leo*5.30}")
+# def Menu():
+#     print("="*8, "CONVERSÃO FODASTICA 3000", "="*8)
+#     print("1 - Converter para DOLAR")
+#     print("2 - Converter para REAL")
+#     print("3 - Sair do programa")
+#     print("="*42)
 
-else: ("\nSAIU DO PROGRAMA")
+# while opcao!=3:
+#     opcao=int(input("\nDigite uma opção: "))
+#     if opcao == 1:
+#         RealDolar()
+#     elif opcao == 2:
+#         DolarReal()
+#     else:
+#         print("OPÇÃO INVÁLIDA")
+    
+
+# else: ("\nSAIU DO PROGRAMA")
        
+#ex03
+saldo=0
+valor=0
+opcao=0
+def Deposito():
+    valor=float(input("Valor do deposito R$ "))
+    global saldo
+    saldo=saldo+valor
+def Saque():
+    valor=float(input("Valor do saque R$ "))
+    global saldo
+    saldo=saldo-valor
+def Extrato():
+    global saldo
+    saldo=saldo+valor
+    print("-"*10, "EXTRATO", "-"*10)
+    print(f"SALDO: R${saldo:.2f}")
+    print("-"*29)
+def Menu():
+    print("="*8, "MATH BANK", "="*  8)
+    print("1 - Realizar Deposito")
+    print("2 - Realizar Saque")
+    print("3 - Emitir Extrato")
+    print("4 - Sair")
+    print("="*42)
+
+Menu()
+while opcao!=4:
+    opcao=int(input("\nDigite a opção: "))
+    if opcao ==1:
+        Deposito()
+    elif opcao ==2:
+        Saque()
+    elif opcao ==3:
+        Extrato()
+    elif opcao!=4: 
+        print("OPÇÃO INVÁLIDA")
+else: print("\nSAIU DO PROGRAMA")
